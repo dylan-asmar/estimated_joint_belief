5bdbbc76ea2cd23b98bb99defb9e35acd13307ec
Dylan Asmar <asmar@stanford.edu>
2024-10-09 17:34:59 -0700

    update/added policy generation script

---
+++ src/policy_generator.jl
@@ -0,0 +1,111 @@
+using POMDPs
+using POMDPTools
+using MultiAgentPOMDPProblems
+using SARSOP
+using DiscreteValueIteration
+using Printf
+# import NativeSARSOP
+using JLD2
+
+using CSV
+using DataFrames
+
+include("problems.jl")
+
+PROBLEMS_TO_RUN = [
+    (:tiger, 60.0),
+    (:tiger_3, 60.0),
+    (:tiger_4, 60.0),
+    (:broadcast, 120.0),
+    (:broadcast_wp, 120.0),
+    (:broadcast_3, 120.0),
+    (:broadcast_3_wp_low, 120.0),
+    (:stochastic_mars, 60.0),
+    (:stochastic_mars_uni_init, 120.0),
+    (:stochastic_mars_big_uni, 300.0),
+    (:box_push, 120.0),
+    (:box_push_obs_05, 300.0),
+    (:joint_meet_2x2, 120.0),
+    (:joint_meet_2x2_13, 120.0),
+    (:joint_meet_3x3, 240.0),
+    (:joint_meet_3x3_wp_uni_init, 300.0),
+    (:joint_meet_big_wp_uni_both, 1200.0),
+    (:joint_meet_big_wp_uni_ls_03, 1800.0),
+    (:wireless, 600.0),
+    (:wireless_wp, 600.0)
+]
+
+function solve_and_save_problem(problem_symbol::Symbol, max_time::Float64)
+
+    joint_problem = get_problem(problem_symbol, 0)
+    agent_problems = [get_problem(problem_symbol, ii) for ii in 1:joint_problem.num_agents]
+
+    @info "Solving MPOMDP for $problem_symbol..."
+    
+    sarsop_solver = SARSOPSolver(; timeout=max_time)
+    
+    joint_policy = solve(sarsop_solver, joint_problem)
+    joint_policy_value = value(joint_policy, initialstate(joint_problem))
+
+    vi_solver = SparseValueIterationSolver(max_iterations=1000, belres=1e-5, verbose=false);
+    joint_mdp_policy = solve(vi_solver, UnderlyingMDP(joint_problem));
+    joint_mdp_policy_value = 0.0;
+    for (si, p) in weighted_iterator(initialstate(joint_problem))
+        joint_mdp_policy_value += p * value(joint_mdp_policy, si)
+    end
+
+    @info "Solving individual policies... "
+    sarsop_solver = SARSOPSolver(; timeout=max_time)
+    agent_policies = Vector{AlphaVectorPolicy}(undef, joint_problem.num_agents)
+    indiv_policy_values = Vector{Float64}(undef, joint_problem.num_agents)
+    for (ii, ap) in enumerate(agent_problems)
+        agent_policies[ii] = solve(sarsop_solver, ap)
+        indiv_policy_values[ii] = value(agent_policies[ii], initialstate(ap))
+    end
+
+    file_name = joinpath("src", "policies", "$problem_symbol.jld2")
+    @info "Saving $file_name"
+    JLD2.@save file_name joint_problem agent_problems agent_policies joint_policy joint_mdp_policy
+    
+    
+    # If CSV for policy values exist, append the results, otherwise create a new CSV
+    csv_file = joinpath("src", "policies", "policy_values.csv")
+    if isfile(csv_file)
+        df = CSV.read(csv_file, DataFrame; types=Dict(:problem => String, :policy => String))
+    else
+        df = DataFrame(
+            problem=String[],
+            policy=String[],
+            solve_time=Float64[],
+            value=Float64[],
+            date=Date[],
+            time=Time[],
+        )
+    end
+    
+    push!(df, (problem=string(problem_symbol), policy="mmdp", solve_time=max_time, value=joint_mdp_policy_value, 
+        date=Date(now()), time=Time(now())))
+    push!(df, (problem=string(problem_symbol), policy="mpomdp", solve_time=max_time, value=joint_policy_value, 
+        date=Date(now()), time=Time(now())))
+    for ii in 1:joint_problem.num_agents
+        push!(df, (problem=string(problem_symbol), policy="pomdp_$ii", solve_time=max_time, value=indiv_policy_values[ii], 
+            date=Date(now()), time=Time(now())))
+    end
+    
+    CSV.write(csv_file, df)
+    
+    @printf("%10s : %10.4f\n", "MMDP", joint_mdp_policy_value)
+    @printf("%10s : %10.4f\n", "MPOMDP", joint_policy_value)
+    for ii in 1:joint_problem.num_agents
+        @printf("%10s : %10.4f\n", "POMDP $ii", indiv_policy_values[ii])
+    end
+    
+    @info "Complete!"
+end
+
+# problem_symbol, max_time = PROBLEMS_TO_RUN[8]
+# solve_and_save_problem(problem_symbol, max_time)
+
+# for (problem_symbol, max_time) in PROBLEMS_TO_RUN
+#     solve_and_save_problem(problem_symbol, max_time)
+# end
---
+++ src/problems.jl
@@ -0,0 +1,102 @@
+using POMDPs
+using MultiAgentPOMDPProblems
+
+PARAM_OPTIONS = Dict(
+    :tiger => (MultiTigerPOMDP, (num_agents=2,)),
+    :tiger_3 => (MultiTigerPOMDP, (num_agents=3,)),
+    :tiger_4 => (MultiTigerPOMDP, (num_agents=4,)), 
+    
+    :broadcast => (BroadcastChannelPOMDP, (num_agents=2,
+        buffer_fill_prob=[0.9, 0.1])),
+    :broadcast_wp => (BroadcastChannelPOMDP, (num_agents=2,
+        buffer_fill_prob=[0.9, 0.1],
+        send_penalty=-0.2)),
+    :broadcast_3 => (BroadcastChannelPOMDP, (num_agents=3,
+        buffer_fill_prob=[0.8, 0.1, 0.1])),
+    :broadcast_3_wp_low => (BroadcastChannelPOMDP, (num_agents=3,
+        buffer_fill_prob=[0.2, 0.4, 0.4],
+        send_penalty=-0.2)),
+    
+    :wireless => (WirelessPOMDP, (num_agents=2,
+        idle_to_packet_prob=0.0470,
+        packet_to_idle_prob=0.0741,
+        discount_factor=0.99)),
+    :wireless_wp => (WirelessPOMDP, (num_agents=2,
+        idle_to_packet_prob=0.0470,
+        packet_to_idle_prob=0.0741,
+        discount_factor=0.99,
+        send_penalty=-0.2)),
+    
+    :stochastic_mars => (StochasticMarsPOMDP, (num_agents=2,
+        map_str="ds\nsd",
+        init_state=StochasticMarsState((1, 1), falses(4)))),
+    :stochastic_mars_uni_init => (StochasticMarsPOMDP, (num_agents=2,
+        map_str="ds\nsd")),
+    :stochastic_mars_big_uni => (StochasticMarsPOMDP, (num_agents=2,
+        map_str="dss\nsdx")),
+    :stochastic_mars_3_uni_init => (StochasticMarsPOMDP, (num_agents=3,
+        map_str="ds\nsd")),
+    
+    :box_push => (BoxPushPOMDP, (num_agents=2,
+        map_option=1)),
+    :box_push_obs_05 => (BoxPushPOMDP, (num_agents=2,
+        map_option=1,
+        observation_prob=0.5)),
+    :box_push_2 => (BoxPushPOMDP, (num_agents=2,
+        map_option=2)),
+    
+    :joint_meet_2x2 => (JointMeetPOMDP, (num_agents=2,
+        map_str="oo\noo",
+        observation_option=:boundaries_lr,
+        init_state=JointMeetState((2, 3)))),
+    :joint_meet_2x2_13 => (JointMeetPOMDP, (num_agents=2,
+        map_str="oo\noo",
+        observation_option=:boundaries_lr,
+        init_state=JointMeetState((1, 3)))),
+    :joint_meet_3x3 => (JointMeetPOMDP, (num_agents=2,
+        map_str="ooo\nooo\nooo",
+        observation_option=:boundaries_both,
+        init_state=JointMeetState((3, 7)),
+        meet_reward_locations=[1, 9])),
+    :joint_meet_3x3_wp_uni_init => (JointMeetPOMDP, (num_agents=2,
+        map_str="ooo\nooo\nooo",
+        observation_option=:boundaries_both,
+        wall_penalty=-1.0)),
+    :joint_meet_big_wp_uni_both => (JointMeetPOMDP, (num_agents=2,
+        map_str="""oxoooxo
+                   oxoooxo
+                   ooooooo
+                   oxoooxo
+                   oxoooxo""",
+        observation_option=:boundaries_both,
+        wall_penalty=-0.1)),
+    :joint_meet_big_wp_uni_ls_03 => (JointMeetPOMDP, (num_agents=2,
+        map_str="""oxoooxo
+                   oxoooxo
+                   ooooooo
+                   oxoooxo
+                   oxoooxo""",
+        observation_option=:left_and_same,
+        wall_penalty=-0.1,
+        observation_sigma=0.3)),
+)
+
+function get_problem(
+    param_option::Symbol,
+    observation_agent::Int;
+    kwparams::NamedTuple=NamedTuple()
+)
+    if !haskey(PARAM_OPTIONS, param_option)
+        throw(ArgumentError("Invalid parameter option: $param_option"))
+    end    
+
+    problem_type = PARAM_OPTIONS[param_option][1]
+    
+    if !isempty(kwparams)
+        @info "Using custom parameters instead of the defined in $param_option"
+    else
+        kwparams = PARAM_OPTIONS[param_option][2]
+    end
+
+    return problem_type(; observation_agent=observation_agent, kwparams...)
+end
